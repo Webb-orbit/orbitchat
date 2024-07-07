0c10aa9fca01a0a058ae9788868787d3933a56b4
webb-orbit <webbenemies@gmail.com>
2024-07-07 17:19:54 +0530

    add glob cool

--- src/compons/Header.jsx
+++ src/compons/Header.jsx
@@ -4,12 +4,16 @@ import Chatbase from '../appwrite/chatbase'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { showt } from '../store/toastslice'
+import createGlobe from "cobe"
+import { useEffect, useRef } from "react"
 
 const Header = () => {
     const { setError, register, handleSubmit, formState: { errors } } = useForm()
     const navi = useNavigate()
     const { userid } = useSelector(state => state.authslice)
     const Dispatch = useDispatch() 
+    const canvasref = useRef(null)
+    
     const createchats = async (data) => {
         try {
             const crete = await Chatbase.createchatbar(data.name, userid)
@@ -21,8 +25,41 @@ const Header = () => {
             setError("root", { message: error.response.message })
         }
     }
+
+    useEffect(()=>{
+        let phi = 0;
+
+        const globe = createGlobe(canvasref.current, {
+          devicePixelRatio: 2,
+          width: 350 * 2,
+          height: 350 * 2,
+          phi: 0,
+          theta: 0,
+          dark: 1,
+          diffuse: 1.2,
+          mapSamples: 16000,
+          mapBrightness: 6,
+          baseColor: [0.3, 0.3, 0.3],
+          markerColor: [0.1, 0.8, 1],
+          glowColor: [1, 1, 1],
+          markers: [
+            { location: [37.7595, -122.4367], size: 0.03 },
+            { location: [40.7128, -74.006], size: 0.1 }
+          ],
+          onRender: (state) => {
+            // Called on every animation frame.
+            // `state` will be an empty object, return updated params.
+            state.phi = phi;
+            phi += 0.01;
+          }
+        });
+    
+        return () => {
+          globe.destroy();
+        };
+    },[])
     return (
-        <div className='h-[15rem] w-full bg-zinc-800 relative select-none flex items-center justify-center py-5'>
+        <div className='h-[20rem] w-full bg-zinc-800 relative select-none flex items-center justify-center py-5'>
             <div className='z-[2] flex flex-col items-center justify-around h-full w-full max-sm:justify-end max-sm:gap-5'>
                 <h1 className='text-[2rem] capitalize font-semibold max-sm:text-[1.5rem]'>create sections of chat</h1>
                 <form onSubmit={handleSubmit(createchats)} className=' flex gap-1 flex-col'>
@@ -36,7 +73,9 @@ const Header = () => {
                         </div>
                 </form>
             </div>
-            <img src={baner} className='z-[1] absolute w-[100%] h-full object-cover object-center ' />
+            <div className="flex absolute w-[100%] h-full justify-center items-center ">
+            <canvas ref={canvasref} className='absolute  h-[100%] w-[100%]'></canvas>
+            </div>
         </div>
     )
 }
